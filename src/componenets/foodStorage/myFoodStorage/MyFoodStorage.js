c69e161a4b1c321b95bd12afd37aca7577acd09f
rushayhays <rushayhays@outlook.com>
2022-04-25 11:17:23 -0500

    structured the MyFoodStorage and MyMealCard pages

--- src/componenets/foodStorage/myFoodStorage/MyFoodStorage.js
+++ src/componenets/foodStorage/myFoodStorage/MyFoodStorage.js
@@ -1,9 +1,28 @@
 //This will load data from the ReserveMeals database, with an expand to include MealPacket Data
 
+import "./myFoodStorage.css"
+
 export const MyFoodStorage = () =>{
     return(
         <>
-            <p>Pretend this is full of tons of Data</p>
+            <section className="myFoodStorageBar">
+                <h4>My Food Storage</h4>
+            </section>
+            <section className="cardDisplayArea">
+                <div className="myFoodStorageCarousel" id="row1"></div>
+                <div className="myFoodStorageCarousel" id="row2"></div>
+                <div className="myFoodStorageCarousel" id="row3"></div>
+                <div className="myFoodStorageCarousel" id="row4"></div>
+            </section>
+            <section className="expirationArea">
+                <div className="expirationBar">
+                    <h4>Expiring Soon!</h4>
+                </div>
+                <div className="expireCardArea">
+
+                </div>
+
+            </section>
         </>
     )
 }
--- src/componenets/foodStorage/myMealCards/MyMealCards.js
+++ src/componenets/foodStorage/myMealCards/MyMealCards.js
@@ -1,9 +1,30 @@
 //This will load the data from the mealPackets Database
 
+import "./myMealCards.css"
+
 export const MyMealCards = () => {
     return(
         <>
-            <p>This too should also be filled with lots of data</p>
+            <section className="topBar">
+                {/* eventually this will also be able to display the creation information */}
+                <h4>My Meal Cards</h4>
+            </section>
+            <section className="mealCardCarousel">
+
+            </section>
+            <section className="mealCardCreation">
+                <div className="mealCreateTitleArea">
+                    <h4>Create a New Meal Card</h4>
+                </div>
+                <div className="mealCreateEntryArea">
+                    <div className="mealCreateEntryBox" id="box1"></div>
+                    <div className="mealCreateEntryBox" id="box2"></div>
+                    <div className="mealCreateEntryBox" id="box3"></div>
+                </div>
+                <div id="createButtonArea">
+                    <h4>Create My Meal Card</h4>
+                </div>
+            </section>
         </>
     )
 }
