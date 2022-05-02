9edeca4968bfec9c04ce09c40a91fcce7b15d4d2
rushayhays <rushayhays@outlook.com>
2022-05-02 00:19:39 -0500

    pie chart calculations

--- src/componenets/foodStorage/foodStorageStats/piecharts/NutritionPieChart.js
+++ src/componenets/foodStorage/foodStorageStats/piecharts/NutritionPieChart.js
@@ -6,9 +6,8 @@ export const NutritionPieChart = () => {
     return(
         <PieChart
             data={[
-                { title: 'One', value: 10, color: '#E38627' },
-                { title: 'Two', value: 15, color: '#C13C37' },
-                { title: 'Three', value: 20, color: '#6A2135' },
+                { title: 'One', value: 100, color: '#E38627' },
+                { title: 'Two', value: 200, color: '#C13C37' },
             ]}
             lineWidth={60}
         />
--- src/componenets/foodStorage/foodStorageStats/piecharts/ProgressPieChart.js
+++ src/componenets/foodStorage/foodStorageStats/piecharts/ProgressPieChart.js
@@ -9,59 +9,72 @@ export const ProgressPieChart = ({parentFoodStorage}) => {
     const[foodstorage, setFoodStorage] = parentFoodStorage
     const[reserveArr, setReserveArr] = useState([])
 
-    let numerator = 50;
-    let denominator = 50;
 
     // value is not in percentages if value1=50 and value2=50
     // then pieChart adds them together to get 100 as the denominator and each 
     //pie portion then covers 50% of that pie
-    let dataValues = [
-        {title: 'One', value: numerator , color: '#E38627'},
-        {title: 'Two', value: denominator , color: '#C13C37'}
-    ]
-
-    useEffect(()=> {
-        getReserveInfo(1).then(infoArray => {
-            setReserveArr(infoArray)
-        })
-        numerator = Math.floor(valuesForPieChart())
-        
-    }, []);
-
+    const[dataValues, setDataValues] = useState([
+        {title: 'One', value:  10, color: '#E38627'},
+        {title: 'Two', value:  10, color: '#C13C37'}
+    ])
+    //Need to figure out how to get a label on here
+    
+    
     const calcCaloriesInMyFoodStorage = () => {
+        
         let CaloriesInMyFoodStorage = 0;
         foodstorage.forEach(meal =>{
-            CaloriesInMyFoodStorage += meal.calories
+            const caloriesAsANum  = parseInt(meal.mealPacket.calories)
+            CaloriesInMyFoodStorage += caloriesAsANum
         })
         return CaloriesInMyFoodStorage
     }
-
+    
     const calcCaloriesNeededToMeetGoal = () =>{
         //This is a constant numder it never needs to change
         const caloriesPerPersonPerDay = 2000;
-
+        
         //This will grab the reserve object and get specific
         //numbers from it
         const reserveObj = reserveArr[0]
-        const peopleInPlan = reserveObj.numOfPeople
-        const numOfDays = reserveObj.reserveGoal
-
+        const peopleInPlan = reserveObj?.numOfPeople
+        console.log("da people" + peopleInPlan)
+        const numOfDays = reserveObj?.goal
+        console.log("da days" + numOfDays)
+        
         //This calculates the calories needed
-        const caloriesNeededToMeetGoal = peopleInPlan * caloriesPerPersonPerDay * numOfDays
-
+        const caloriesNeededToMeetGoal = parseInt(peopleInPlan) * parseInt(caloriesPerPersonPerDay) * parseInt(numOfDays)
+        console.log("how many c do you need" + caloriesNeededToMeetGoal)
         return caloriesNeededToMeetGoal
     }
+    
+    const valuesForPieChart = (myCurrentCalories) =>{
+        const calorieGoal = calcCaloriesNeededToMeetGoal();
+        console.log("this is the Goal "+calorieGoal)
 
-    const valuesForPieChart = () =>{
-        const currentCalories = calcCaloriesInMyFoodStorage();
-        const neededCalories = calcCaloriesNeededToMeetGoal();
-
-        const numerator = ((currentCalories/neededCalories)*100)
-
-        return numerator
+        
+        const remainingCaloriesNeeded = parseInt(calorieGoal) - parseInt(myCurrentCalories)
+        console.log(remainingCaloriesNeeded)
+        const percentageCompletion = Math.floor((myCurrentCalories/calorieGoal)*100)
+   
+        let valueArr = [
+            {title: 'One', value: myCurrentCalories, color: '#E38627'},
+            {title: 'Two', value: remainingCaloriesNeeded, color: '#C13C37'}
+        ]
+        
+        setDataValues(valueArr)
     }
-
-
+    
+    
+    useEffect(()=> {
+        getReserveInfo(1).then(infoArray => {
+            setReserveArr(infoArray)
+            const myCalories = calcCaloriesInMyFoodStorage()
+            //set my calories as a useState?
+            valuesForPieChart(myCalories)
+        })
+                
+    }, []);
 
 
     return(
